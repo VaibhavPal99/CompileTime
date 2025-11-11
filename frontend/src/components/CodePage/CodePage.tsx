2c1ec8a47a57bffbe509f67c879d844a9146b1e9
VaibhavPal99 <vaibhavpal9935@gmail.com>
2025-11-11 18:04:51 +0530

    fixed error

--- frontend/src/components/CodePage/CodePage.tsx
+++ frontend/src/components/CodePage/CodePage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Editor from "@monaco-editor/react";
 import "./CodePage.css";
 import NoteModal from "../Note/NoteModal";
